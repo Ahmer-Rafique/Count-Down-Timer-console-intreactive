1f905eb12923a60d13582abca3ea8a7f9ddc990a
AhmerAli17 <ahmerali1771@gmail.com>
2023-09-14 21:01:34 +0100

    update

--- index.js
+++ index.js
@@ -3,7 +3,18 @@ import inquirer from "inquirer";
 const res = await inquirer.prompt({
     type: "number",
     name: "userInput",
-    message: "please Enter the amount second"
+    message: "please Enter the amount second",
+    validate: (input) => {
+        if (isNaN(input)) {
+            return "please enter valid number";
+        }
+        else if (input > 100) {
+            return "must be second in 100";
+        }
+        else {
+            return true;
+        }
+    }
 });
 let input = res.userInput;
 function startTime(val) {
@@ -18,8 +29,8 @@ function startTime(val) {
         }
         const min = Math.floor((timeDiff % (3600 * 24)) / 3600);
         const sec = Math.floor(timeDiff % 60);
-        console.log(`${min}: ${sec}`);
-    }), 1000;
+        console.log(`${min.toString().padStart(2, "0")}: ${sec}`);
+    }, 1000);
 }
 ;
 startTime(input);
